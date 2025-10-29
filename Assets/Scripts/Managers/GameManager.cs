c7e9494970a9301b14b90a11726a0ca1004b400a
bzucchi99 <bzucchi99@gmail.com>
2025-10-29 17:28:00 -0300

    Modificacion de scripts para audio. Implementacion de audio manager y sonidos.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         UIManager.Instance.ShowPanel(initPanel);
+        AudioManager.instance.Play("Menu");
     }
     private void StartSingleton()
     {
